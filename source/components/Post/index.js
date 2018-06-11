30c543daf8c208a374c05014cc598cb3a770e3d3
Maxym Ruzmetov <maxruzmetov@gmail.com>
2018-06-11 08:34:08 +0300

    Add files via upload
    Add spinner

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,34 +1,37 @@
 // Core
 import React, { Component } from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
 // Components
-import { Consumer } from 'components/HOC/withProfile'
+import { Consumer } from 'components/HOC/withProfile';
 
 //Insruments
 import Styles from './styles.m.css';
 
-
 export default class Post extends Component {
+    static propTypes = {
+        comment: PropTypes.string.isRequired,
+        created: PropTypes.number.isRequired,
+    };
     render () {
-        const {
-            currentUserFirstName,
-            currentUserLastName,
-            avatar,
-        } = this.props;
+        const { comment, created } = this.props;
 
         return (
             <Consumer>
                 {(context) =>
                     <section className = { Styles.post }>
                         <img src = { context.avatar } />
-                        <a>{`${ context.currentUserFirstName } ${ context.currentUserLastName }`}</a>
-                        <time>{moment().format('MMMM D h:ss a')}</time>
-                        <p>Howdy!</p>
+                        <a>{`${ context.currentUserFirstName } ${ 
+                            context.currentUserLastName 
+                        }`}</a>
+                        <time>
+                            {moment.unix(created).format('MMMM D h:ss a')}
+                        </time>
+                        <p>{comment}</p>
                     </section>
                 }
             </Consumer>
-            
         );
     }
 }
